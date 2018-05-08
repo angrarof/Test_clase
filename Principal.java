47760753114d24ba292bf73ba03eb186c626e46a
angrarof <angel.aguilar0@hotmail.com>
2018-05-07 20:38:27 -0500

    Se coloco error en Principal.java

--- Principal.java
+++ Principal.java
@@ -3,7 +3,7 @@ public class Principal{
 	public static void main(String[] args){
 
 		for(int i=1;i<=10;i++){
-			System.out.println("Linea ...."+i);
+			System.out.println("Linea ...."+i)
 		}	
 	
 	}
